6721a09326ba6db830f0f26b849761d485722757
Lucas Munhoz <153121057+LucasMunhoz13@users.noreply.github.com>
2025-01-21 21:32:45 -0700

    Final Version Assignment 2

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,14 @@
+import StudentInfo from "./student-info.js";
+import Link from "next/link";
+
 export default function Page() {
     return (
       <main>
         <h1>Shopping List</h1>
+        <StudentInfo/>
+        <p>
+            <Link href="https://github.com/">https://github.com/</Link>
+        </p>
       </main>
     );
   }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,3 @@
+export default function StudentInfo() {
+    return <h1>Lucas Munhoz da Costa</h1>
+}

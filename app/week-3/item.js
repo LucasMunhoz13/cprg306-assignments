ac5800474d978be9029bc1f0a1b2474954a6b942
LucasMunhoz13 <lucas1922@gmail.com>
2025-02-01 17:03:02 -0700

    Few adjustments

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,9 +1,8 @@
 export default function Item({ name, quantity, category }) {
     return (
-      <li className="border-2 border-black bg-sky-600 p-4 m-4">
+      <li className="border-2 border-black bg-sky-600 p-2 m-1 max-w-md">
         <h2 className="font-bold text-lg">{name}</h2>
-        <p className="ml-1">Quantity: {quantity}</p>
-        <p className="ml-1">Category: {category}</p>
+        <p className="ml-1">Buy {quantity} in {category}</p>
       </li>
     );
   }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list.js";
 export default function Page() {
     return (
       <main>
-        <h1 className="text-4xl font-bold m-4">Shopping List</h1>
+        <h1 className="text-4xl font-bold m-7">Shopping List</h1>
         <ItemList />
       </main>
     );

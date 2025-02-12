503a07f745055fd5ffc95836b544bcfd3342c87a
LucasMunhoz13 <lucas1922@gmail.com>
2025-02-11 18:46:14 -0700

    Final version

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,20 +1,31 @@
 "use client";
 
-import {useState} from "react";
+import { useState } from "react";
 
 export default function NewItem() {
-    const [count, setCount] = useState(0);
+    const [quantity, setQuantity] = useState(1);
 
-    const increment = () => setCount(count + 1);
+    const increment = () => setQuantity(prevQuantity => Math.min(prevQuantity + 1, 20));
 
-    const decrement = () => setCount(count - 1);
-
+    const decrement = () => setQuantity(prevQuantity => Math.max(prevQuantity - 1, 1));
 
     return (
         <div>
-            <p className="flex">Count: {count}</p>
-                <button onClick={decrement} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 m-1 rounded">-</button>
-                <button onClick={increment} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 m-1 rounded">+</button>
+            <p className="">Quantity: {quantity}</p>
+            <button 
+                onClick={decrement} 
+                className={`font-bold py-1 px-3 m-1 rounded ${quantity === 1 ? 'bg-gray-500 text-gray-300' : 'bg-blue-500 hover:bg-blue-700 text-white'}`}
+                disabled={quantity === 1}
+            >
+                -
+            </button>
+            <button 
+                onClick={increment} 
+                className={`font-bold py-1 px-3 m-1 rounded ${quantity === 20 ? 'bg-gray-500 text-gray-300' : 'bg-blue-500 hover:bg-blue-700 text-white'}`}
+                disabled={quantity === 20}
+            >
+                +
+            </button>
         </div>
-    )
+    );
 }
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,8 +2,9 @@ import NewItem from "./new-item.js";
 
 export default function Page() {
     return (
-      <main>
-        <NewItem />
-      </main>
+        <main className="bg-gray-100 min-h-screen">
+            <h1 className="text-4xl font-bold m-7">Add New Item</h1>
+            <NewItem />
+        </main>
     );
 }

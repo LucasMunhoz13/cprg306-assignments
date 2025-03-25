46e6608a13db40f7c14104e09470507c624a0a98
LucasMunhoz13 <lucas1922@gmail.com>
2025-03-24 20:15:39 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -25,6 +25,9 @@ export default function Page () {
       <p className="text-lg hover:text-green-500 pl-32">
         <Link href="week-8"> Week 8 Assignment</Link>
       </p>
+      <p className="text-lg hover:text-green-500 pl-32">
+        <Link href="week-9"> Week 9 Assignment</Link>
+      </p>
     </div>
   );
 }
